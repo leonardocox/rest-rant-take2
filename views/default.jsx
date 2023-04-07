1f4a589cb44e96658338a9a0a6802b86866262d2
leonardocox <leonardocx0@gmail.com>
2023-04-07 10:51:08 -0400

    spiced and added more to home and default jsx

--- views/default.jsx
+++ views/default.jsx
@@ -1,26 +1,52 @@
-const React = require('react')
+const React = require("react");
 
-function Def (html) {
+const Def = (html) => {
   return (
-      <html>
-          <head>
-              <title>Title</title>
-              <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css' />
-              <link rel='stylesheet' href='/css/style.css' />
-          </head>
-          <body>
-            <nav className='container red top-bottom-borders-in'>
-                <div>
-                    <a href='/'>Home</a>
-                    <a href='/places'>Places</a>
-                    <a href='/places/new'>Add Place</a>
-                </div>
-            </nav>
-              {html.children}
-          </body>
-      </html>
-  )
-}
+    <html>
+      <head>
+        {/* <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+                <title>Rest-rant Review App</title>*/}
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+          crossOrigin="anonymous"
+        ></link>
+        <link rel="stylesheet" href="/css/styles.css"></link>
+      </head>
+      <body>
+        <div id="navBar">
+          <ul>
+            <li>
+              <a href="/">
+                <button type="button" id="homeBtn" className="btn-primary">
+                  Home Page
+                </button>
+              </a>
+            </li>
+            <li>
+              <a href="/places">
+                <button type="button" id="homePlaceBtn" className="btn-primary">
+                  Places Page
+                </button>
+              </a>
+            </li>
+            <li>
+              <a href="/places/newPlace">
+                <button type="button" id="newPlaceBtn" className="btn-primary">
+                  New Place Page
+                </button>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="content">{html.children}</div>
+        <footer className="footer-footer">My Links go here.</footer>
+      </body>
+    </html>
+  );
+};
 
-
-module.exports = Def
+module.exports = Def;
--- views/home.jsx
+++ views/home.jsx
@@ -1,25 +1,63 @@
-const React = require('react')
-const Def = require('./default')
+const React = require("react");
+const Def = require("./default");
 
-function home () {
-    return (
-        <Def>
-            <main>
-                <h1>REST-Rant</h1>
-                <div>
-                    <img className='ramen-home' src='/images/ramen.jpg' alt='Ramen'/>
-                    <div className='ramen-home'>
-                        Photo by <a href="https://unsplash.com/@susannschuster?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Susann Schuster</a> on <a href="https://unsplash.com/s/photos/ramen-noodle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-                    </div>
-                </div>
-                <div className='fill-button'>
-                <a href='/places'>
-                    <button type="button" className='btn-primary'>Places Page</button>
-                </a>
-                </div>
-            </main>
-        </Def>
-    )
-}
+const home = () => {
+  return (
+    <Def title="REST-Rant Home">
+      <title>REST-Rant Home</title>
+      <main>
+        <h1>REST-Rant Home Page</h1>
+        {/* <div id="navBar">
+                        <a href="/places">
+                            <button id="homePlaceBtn" className="btn-primary">Places Page</button>
+                        </a>
+                        <a href="/places/newPlace">
+                            <button id="newPlaceBtn" className="btn-primary">New Place Page</button>
+                        </a>
+                    </div> */}
+        <div id="homeImageContainer" className="container">
+          <div className="row-1">
+            <div className="col-1">
+              <img
+                className="homeImage"
+                src="/images/steak.jpg"
+                alt="Steak Picture"
+              ></img>
+              <a
+                className="homeLink"
+                href="https://pixabay.com/photos/steak-sirloin-meat-beef-food-1083567/"
+              >
+                Image by dbreen on Pixaby
+              </a>
+            </div>
+            <div className="col-2">
+              <img
+                className="homeImage"
+                src="/images/chicken-wings.jpg"
+                alt="Wings Picture"
+              ></img>
+              <a
+                className="homeLink"
+                href="https://www.freepik.com/photos/buffalo-wings"
+              >
+                Buffalo wings photo created by timolina - www.freepik.com
+              </a>
+            </div>
+          </div>
+          {/* <div className="row-2">
+                            <div className="col-1">
+                                <img className="homeImage" src="/images/     " alt=" Picture"></img>
+                                <a className="homeLink" href=""></a>
+                            </div>
+                            <div className="col-2">
+                                <img className="homeImage" src="/images/     " alt=" Picture"></img>
+                                <a className="homeLink" href=""></a>
+                            </div> 
+                        </div> */}
+        </div>
+      </main>
+    </Def>
+  );
+};
 
-module.exports = home
+module.exports = home;
