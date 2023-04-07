2fb910397cce1fb5452d966d1cdddacb6e9d2a3b
leonardocox <leonardocx0@gmail.com>
2023-04-07 10:56:54 -0400

    installed method-override and fixed edit route

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-// Modules and Globals
-require("dotenv").config();
+//Modules and Globals
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv").config();
+const methodOverride = require("method-override");
 
-// Express Settings
+//Express Settings
+app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-// Controllers and Routes
+//Controllers and Routes
 app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
+//Wildcard Route
 app.get("*", (req, res) => {
-  res.render("error404");
+  res.status(404).render("error404");
 });
 
-// Listen for Connections
-app.listen(process.env.PORT);
+//Listen for Connections
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on ${process.env.PORT}`)
+);
